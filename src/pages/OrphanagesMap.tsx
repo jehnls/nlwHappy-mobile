8995ec3191bcd36f51f2b60608e1a3d31a4685ac
JehNls <jeferson_nls@hotmail.com>
2020-10-18 23:48:36 -0300

    If applied this commit will 4ª class app finalized, Create flow the register orphanage; Add view lacking; Details of the Orphanage; Create Orphanage

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,18 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, Dimensions,  } from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
 
 import mapMarker from '../images/map-marker.png';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+
+import { RectButton } from 'react-native-gesture-handler';
+
+import api from '../services/api';
+
+
+interface Orphanages {
+    id: number;
+    name: string;
+    latitude: number;
+    longitude: number;
+}
 
 export default function OrphanagesMap() {
+    const [orphanages, setOrphanages] = useState<Orphanages[]>([]);
     const navigation = useNavigation();
 
-    function hadleNavigateToOrphanageDetails() {
-        navigation.navigate('OrphanageDetails');
+    useFocusEffect(() => {
+        api.get('orphanages').then(response =>{
+            setOrphanages(response.data);
+        });
+    },);
+
+    function hadleNavigateToOrphanageDetails(id: number) {
+        navigation.navigate('OrphanageDetails', { id });
     }
 
+    function hadleNavigateToOrphanageOrphanage() {
+        navigation.navigate('SelectMapPosition');
+    }
+    
     return (
         <View style={styles.container}>
             <MapView
@@ -25,31 +48,37 @@ export default function OrphanagesMap() {
                     longitudeDelta: 0.008,
                 }}
             >
-                <Marker
+                {orphanages.map(orphanage => {
+                    
+                    return (
+                        <Marker
+                        key={orphanage.id}
                     icon={mapMarker}
                     calloutAnchor={{
                         x: 2.7,
                         y: 0.8,
                     }}
                     coordinate={{
-                        latitude: -23.6904425,
-                        longitude: -46.6373211,
+                        latitude: orphanage.latitude,
+                        longitude: orphanage.longitude,
                     }}
                 >
-                    <Callout tooltip onPress={hadleNavigateToOrphanageDetails}>
+                    <Callout tooltip onPress={() =>hadleNavigateToOrphanageDetails(orphanage.id)}>
                         <View style={styles.calloutContainer}>
-                            <Text style={styles.calloutText}>Lar das meninas</Text>
+                            <Text style={styles.calloutText}>{orphanage.name}</Text>
                         </View>
                     </Callout>
                 </Marker>
+                    );
+                })}
             </MapView>
 
             <View style={styles.footer}>
-                <Text style={styles.footerText}>2 Orfanatos encontrados</Text>
+                <Text style={styles.footerText}>{orphanages.length} orfanatos encontrados</Text>
 
-                <TouchableOpacity style={styles.createOrphanageButton} onPress={() => { }}>
+                <RectButton style={styles.createOrphanageButton} onPress={hadleNavigateToOrphanageOrphanage}>
                     <Feather name="plus" size={20} color="#FFF" />
-                </TouchableOpacity>
+                </RectButton>
             </View>
         </View>
     )
